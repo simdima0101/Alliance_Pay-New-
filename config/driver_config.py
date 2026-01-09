5c27512477d6fa77799d0b46563d6352a5aa39cb
DMITRIY SIM <user@MacBook-Air.local>
2026-01-09 12:53:12 +0500

    driver config changes

--- config/driver_config.py
+++ config/driver_config.py
@@ -5,12 +5,17 @@ def android_options():
     options = UiAutomator2Options()
 
     options.platform_name = "Android"
-    options.device_name = "Android Emulator"
+    options.device_name = "Pixel 8 Pro"
     options.automation_name = "UiAutomator2"
 
-    options.app_package = ""
-    options.app_activity = ""
+    options.device_name = ""   # adb devices
+    options.udid = ""
+
+    options.app_package = "uz.tune.juicer"
+    options.app_activity = "uz.tune.juicer.ui.activity.main.MainActivityDefault"
+    options.app_wait_activity = "*"
 
     options.no_reset = True
+    options.new_command_timeout = 300
 
     return options
